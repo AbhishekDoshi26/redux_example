b45478ebf0bf4f7c10e042ad7a8179103fa2394b
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2021-09-21 12:30:13 +0530

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+import 'package:redux_example/redux.dart';
+
+class Home extends StatelessWidget {
+  Home({Key? key}) : super(key: key);
+  final store = Store<int>(ReduxActions.counterUpdate, initialState: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: StoreConnector<int, String>(
+        converter: (store) => store.state.toString(),
+        builder: (context, count) {
+          return Text(
+            'The button has been pushed this many times: $count',
+            style: Theme.of(context).textTheme.headline4,
+          );
+        },
+      )),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          StoreConnector<int, VoidCallback>(
+            converter: (store) {
+              return () => store.dispatch(CounterActions.Increment);
+            },
+            builder: (context, callback) {
+              return IconButton(
+                onPressed: callback,
+                icon: Icon(Icons.add),
+              );
+            },
+          ),
+          StoreConnector<int, VoidCallback>(
+            converter: (store) {
+              return () => store.dispatch(CounterActions.Decrement);
+            },
+            builder: (context, callback) {
+              return IconButton(
+                onPressed: callback,
+                icon: Icon(Icons.remove),
+              );
+            },
+          ),
+          StoreConnector<int, VoidCallback>(
+            converter: (store) {
+              return () => store.dispatch(CounterActions.Reset);
+            },
+            builder: (context, callback) {
+              return IconButton(
+                onPressed: callback,
+                icon: Icon(Icons.refresh),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux_example/home.dart';
+import 'package:redux/redux.dart';
+import 'package:redux_example/redux.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StoreProvider<int>(
+      store: Store<int>(ReduxActions.counterUpdate, initialState: 0),
+      child: MaterialApp(
+        title: 'Material App',
+        home: Home(),
+      ),
+    );
+  }
+}
