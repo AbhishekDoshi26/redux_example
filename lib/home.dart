b2ea95db1ba535d93a23bed96b7429f85d689e9f
AbhishekDoshi26 <adoshi26.ad@gmail.com>
2021-09-23 19:32:27 +0530

    updated home screen

--- lib/home.dart
+++ lib/home.dart
@@ -12,52 +12,57 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: StoreConnector<int, String>(
-        converter: (store) => store.state.toString(),
-        builder: (context, count) {
-          return Text(
-            'The button has been pushed this many times: $count',
-            style: Theme.of(context).textTheme.headline4,
-          );
-        },
-      )),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          StoreConnector<int, VoidCallback>(
-            converter: (store) {
-              return () => store.dispatch(CounterActions.Increment);
-            },
-            builder: (context, callback) {
-              return IconButton(
-                onPressed: callback,
-                icon: Icon(Icons.add),
-              );
-            },
-          ),
-          StoreConnector<int, VoidCallback>(
-            converter: (store) {
-              return () => store.dispatch(CounterActions.Decrement);
-            },
-            builder: (context, callback) {
-              return IconButton(
-                onPressed: callback,
-                icon: Icon(Icons.remove),
-              );
-            },
-          ),
-          StoreConnector<int, VoidCallback>(
-            converter: (store) {
-              return () => store.dispatch(CounterActions.Reset);
-            },
-            builder: (context, callback) {
-              return IconButton(
-                onPressed: callback,
-                icon: Icon(Icons.refresh),
-              );
-            },
-          ),
-        ],
+        child: StoreConnector<int, String>(
+          converter: (store) => store.state.toString(),
+          builder: (context, count) {
+            return Text(
+              'The button has been pushed this many times: $count',
+              style: Theme.of(context).textTheme.headline4,
+            );
+          },
+        ),
+      ),
+      floatingActionButton: Container(
+        color: Colors.red,
+        height: 50.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            StoreConnector<int, VoidCallback>(
+              converter: (store) {
+                return () => store.dispatch(CounterActions.Increment);
+              },
+              builder: (context, callback) {
+                return IconButton(
+                  onPressed: callback,
+                  icon: Icon(Icons.add),
+                );
+              },
+            ),
+            StoreConnector<int, VoidCallback>(
+              converter: (store) {
+                return () => store.dispatch(CounterActions.Decrement);
+              },
+              builder: (context, callback) {
+                return IconButton(
+                  onPressed: callback,
+                  icon: Icon(Icons.remove),
+                );
+              },
+            ),
+            StoreConnector<int, VoidCallback>(
+              converter: (store) {
+                return () => store.dispatch(CounterActions.Reset);
+              },
+              builder: (context, callback) {
+                return IconButton(
+                  onPressed: callback,
+                  icon: Icon(Icons.restart_alt),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
